b8875c0566788871caf8c1848591b9c3afa663e3
bschels <93496710+bschels@users.noreply.github.com>
2022-05-10 17:42:03 +0200

    Update ajax.js

--- js/ajax.js
+++ js/ajax.js
@@ -1,8 +1,5 @@
-<script src="jquery.js" type="text/javascript"></script> 
-<script>
-	function kb_source_2_target() {
+function kb_source_2_target() {
 		$.get('/pages/datenschutz.html', function(data) {
 			$('#target').html(data);	
 		})
 	}
-</script>
