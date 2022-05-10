472b6d002ad08771620da9c4754f36d973bef7bd
bschels <93496710+bschels@users.noreply.github.com>
2022-05-10 20:45:46 +0200

    Update ajax.js

--- js/ajax.js
+++ js/ajax.js
@@ -1,5 +1,15 @@
-function kb_source_2_target() {
+function kb_source_2_datenschutz() {
 		$.get('/pages/datenschutz.html', function(data) {
+			$('#target').html(data);	
+		})
+	}
+function kb_source_2_impressum() {
+		$.get('/pages/impressum.html', function(data) {
+			$('#target').html(data);	
+		})
+	}
+function kb_source_2_cv() {
+		$.get('/pages/cv.html', function(data) {
 			$('#target').html(data);	
 		})
 	}
