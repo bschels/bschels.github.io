2d5c7fed5a2625491df7cb55857431eba732b6a1
bschels <93496710+bschels@users.noreply.github.com>
2022-05-12 12:07:47 +0200

    Delete switcher.js

--- js/switcher.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $(".switch-language").on("click", function() {
-    var switchTo = $(this).attr("id");
-    $(".language").removeClass('active');
-    $(".language#" + switchTo).addClass('active');
-  });
-});
