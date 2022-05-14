53b177863e6fe7aa10ec7f9e142d21d6fe3850e4
bschels <93496710+bschels@users.noreply.github.com>
2022-05-14 14:17:25 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -51,5 +51,24 @@ toggle.onclick = function() {
 };
 };
 
+// color changer 
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+const btn = document.getElementById("heart");
+const clr = document.querySelector("body");
+document.getElementById("hex");
+
+btn.addEventListener("click", () => {
+ document.documentElement.style.setProperty('--primary', getRandomColor());
+
+});
+
 
 
