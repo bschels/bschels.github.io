d064a0fabb235301a1d65d68b497f6221e280d36
bschels <93496710+bschels@users.noreply.github.com>
2022-05-18 16:16:47 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -29,7 +29,8 @@ $(document).ready(function() {
 
 window.onload = function(){ 
 var toggle = document.getElementById("theme-toggle");
-
+var toggle = document.getElementById("theme-toggle2");
+	
 var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
 if (storedTheme)
     document.documentElement.setAttribute('data-theme', storedTheme)
