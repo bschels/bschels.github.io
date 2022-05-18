93ccdda3f06e37b2f0092bc89fabe408277ead4a
bschels <93496710+bschels@users.noreply.github.com>
2022-05-18 17:52:24 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -68,9 +68,24 @@ function myFunction_set() {
 }
 
 function darkmode() {
-  var toggle
-}
+function(){ 
+var toggle = document.getElementsByClassName("theme-toggle");
+	
+var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+if (storedTheme)
+    document.documentElement.setAttribute('data-theme', storedTheme)
 
-function darkmode2() {
-  var toggle
+
+toggle.onclick = function() {
+    var currentTheme = document.documentElement.getAttribute("data-theme");
+    var targetTheme = "light";
+
+    if (currentTheme === "light") {
+        targetTheme = "dark";
+    }
+
+    document.documentElement.setAttribute('data-theme', targetTheme)
+    localStorage.setItem('theme', targetTheme);
+};
+};
 }
