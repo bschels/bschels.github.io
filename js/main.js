5f43f54bb8028e404cd27a17876f6d34764395ee
bschels <93496710+bschels@users.noreply.github.com>
2022-05-18 17:59:17 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -68,24 +68,4 @@ function myFunction_set() {
 }
 
 function darkmode() {
-function(){ 
-var toggle = document.getElementsByClassName("theme-toggle");
-	
-var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
-if (storedTheme)
-    document.documentElement.setAttribute('data-theme', storedTheme)
-
-
-toggle.onclick = function() {
-    var currentTheme = document.documentElement.getAttribute("data-theme");
-    var targetTheme = "light";
-
-    if (currentTheme === "light") {
-        targetTheme = "dark";
-    }
-
-    document.documentElement.setAttribute('data-theme', targetTheme)
-    localStorage.setItem('theme', targetTheme);
-};
-};
 }
