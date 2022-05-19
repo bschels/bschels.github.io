bfc0cf2f94bbf26fe2dedc48b4c82ed79ac62772
Benni <93496710+bschels@users.noreply.github.com>
2022-05-19 19:12:38 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -58,9 +58,8 @@ var letters = "0123456789ABCDEF";
 var color = "#";
 for (var i = 0; i < 6; i++) {
 color += letters[Math.floor(Math.random() * 14)];
-return color;
-	
 }
+return color;
 };
 // Get the root element
 var r = document.querySelector(':root');
