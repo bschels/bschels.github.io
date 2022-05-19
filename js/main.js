3c4b75f1786312ecac04cb325fdf5c95371e06e7
Benni <93496710+bschels@users.noreply.github.com>
2022-05-19 18:23:02 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -67,7 +67,7 @@ function getRandomColor() {
 var letters = "0123456789ABCDEF";
 var color = "#";
 for (var i = 0; i < 6; i++) {
-color += letters[Math.floor(Math.random() * 16)];
+color += letters[Math.floor(Math.random() * 14)];
 
 return color;
 	
