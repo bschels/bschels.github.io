4b96f8aeaa433be0baf60074caa168c61a6db4d8
Benni <93496710+bschels@users.noreply.github.com>
2022-05-19 17:49:28 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -50,27 +50,26 @@ toggle.onclick = function() {
 
 // color switcher
 
+function getRandomColor() {
+	
 // //alt: dark colors
-//function getRandomColor() {
 //let color = "#";
 //for (let i = 0; i < 3; i++)
 //color += ("0" + Math.floor(Math.random() * Math.pow(16, 2) / 2).toString(16)).slice(-2);
-//return color;
 
 // //alt: all colors
 //var letters = "0123456789ABCDEF";
 //var color = "#";
 //for (var i = 0; i < 6; i++) {
 //color += letters[Math.floor(Math.random() * 16)];
-//}
-//return color;
-
-// //alt: nice colors
+	
+//nice colors
 function getRandomColor() {
 
 let color = "#";
 for (let i = 0; i < 4; i++)
 color += ("0" + Math.floor(Math.random() * Math.pow(16, 2) / 2).toString(16)).slice(-2);
+
 return color;
 	
 }
