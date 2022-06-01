21a1c9869d1a44412bb64b94f802a43fd1b83c94
Benni <93496710+bschels@users.noreply.github.com>
2022-06-01 18:04:28 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -117,21 +117,12 @@ $('#fade').on('click', function(event) {
 	});
 
 // Radio Button Closer
-
-var radioButtons = $("input[type='radio']");
-var radioStates = {};
-$.each(radioButtons, function(index, rd) {
-    radioStates[rd.value] = $(rd).is(':checked');
-});
-
-radioButtons.click(function() {
-    
-    var val = $(this).val();  
-    $(this).attr('checked', (radioStates[val] = !radioStates[val]));    
-    
-    $.each(radioButtons, function(index, rd) {
-        if(rd.value !== val) {
-            radioStates[rd.value] = false; 
-        }
+$(document).ready(function(){
+    $("input:radio:checked").data("chk",true);
+    $("input:radio").click(function(){
+        $("input[name='"+$(this).attr("name")+"']:radio").not(this).removeData("chk");
+        $(this).data("chk",!$(this).data("chk"));
+        $(this).prop("checked",$(this).data("chk"));
+        $(this).button('refresh'); // in case you change the radio elements dynamically
     });
 });
