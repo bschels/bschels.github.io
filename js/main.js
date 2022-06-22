56c4efa2df2436217e26bf06c3121d338825fe06
Benni <93496710+bschels@users.noreply.github.com>
2022-06-22 11:57:54 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -126,3 +126,23 @@ $(document).ready(function(){
         $(this).button('refresh'); // in case you change the radio elements dynamically
     });
 });
+
+// Thumbnail enlarger
+
+$(document).ready(function(thumb_enlarger) 
+      // Get the img object using its Id
+      img = document.getElementById("thumb");
+      // Function to increase image size
+      function enlargeImg() {
+        // Set image size to 1.5 times original
+        img.style.transform = "scale(1.5)";
+        // Animation effect
+        img.style.transition = "transform 0.25s ease";
+      }
+      // Function to reset image size
+      function resetImg() {
+        // Set image size to original
+        img.style.transform = "scale(1)";
+        img.style.transition = "transform 0.25s ease";
+      });
+
