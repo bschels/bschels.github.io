99a649adb6990f81ebf1edfbf1f65e111ce70a72
Benni <93496710+bschels@users.noreply.github.com>
2022-06-22 15:15:33 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -127,27 +127,3 @@ $(document).ready(function(){
     });
 });
 
-// Thumbnail enlarger
-
-
-$( document ).ready(function imgEnlarge(e){
-
-            if(e.style.width=="300px" || e.style.width=="" ){
-                e.style.width="400px";
-            }
-            else{
-                e.style.width="300px";
-            }
-
-            var imgLength = document.getElementsByTagName('img').length;
-
-            for(var i = 0; i<imgLength; i++){
-                if(document.getElementsByTagName('img')[i].id==  e.id){
-                    continue;
-                }
-                document.getElementsByTagName('img')[i].style.width="300px";
-
-            }
-        
-});
-
