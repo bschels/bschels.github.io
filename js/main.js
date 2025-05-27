efeece23686077874f25091df37a64c1a4e6ded8
Benni <93496710+bschels@users.noreply.github.com>
2025-05-27 14:28:52 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,59 +1,72 @@
-// Preloader
-$(window).on('load', function() {
-  $('#status').fadeOut();
-  $('#preloader').delay(350).fadeOut('slow');
-  $('body').delay(550).css({'overflow':'visible'});
-});
+$(function() {
+  // Preloader
+  $(window).on('load', function() {
+    $('#status').fadeOut();
+    $('#preloader').delay(350).fadeOut('slow');
+    $('body').delay(550).css({'overflow':'visible'});
+  });
 
-// AJAX Load
-function kb_source_2_datenschutz() {
-  $.get('/pages/datenschutz.html', function(data) {
-    $('#datenschutz').html(data);	
-  });
-}
-function kb_source_2_impressum() {
-  $.get('/pages/impressum.html', function(data) {
-    $('#impressum').html(data);	
-  });
-}
-function kb_source_2_cv() {
-  $.get('/pages/cv.html', function(data) {
-    $('#cv').html(data);	
-  });
-}
-function kb_source_2_profil() {
-  $.get('/pages/profil.html', function(data) {
-    $('#profil').html(data);	
-  });
-}
-function kb_source_2_leistungen() {
-  $.get('/pages/leistungen.html', function(data) {
-    $('#leistungen').html(data);	
-  });
-}
-function kb_source_2_portfolio() {
-  $.get('/pages/projekte.html', function(data) {
-    $('#projekte').html(data);	
-  });
-}
-function kb_source_2_bauenimbestand() {
-  $.get('/pages/bauenimbestand.html', function(data) {
-    $('#bauenimbestand').html(data);	
-  });
-}
-function kb_source_2_design() {
-  $.get('/pages/design.html', function(data) {
-    $('#design').html(data);	
-  });
-}
-
-// Language switcher
-$(document).ready(function() {
+  // Language switcher
   $(".switch-language").on("click", function() {
     var switchTo = $(this).attr("id");
     $(".language").removeClass('active');
     $(".language#" + switchTo).addClass('active');
   });
+
+  // Lightbox Hider on any click outside
+  $('#fade').on('click', function(event) {
+    $(".white_content, #fade").hide();
+  });
+
+  // Radio Button Closer: Accordion
+  $("input:radio:checked").data("chk", true);
+  $("input:radio").click(function() {
+    $("input[name='" + $(this).attr("name") + "']:radio").not(this).removeData("chk");
+    $(this).data("chk", !$(this).data("chk"));
+    $(this).prop("checked", $(this).data("chk"));
+  });
+
+  // AJAX Loader
+  window.kb_source_2_datenschutz = function() {
+    $.get('/pages/datenschutz.html', function(data) {
+      $('#datenschutz').html(data);
+    });
+  };
+  window.kb_source_2_impressum = function() {
+    $.get('/pages/impressum.html', function(data) {
+      $('#impressum').html(data);
+    });
+  };
+  window.kb_source_2_cv = function() {
+    $.get('/pages/cv.html', function(data) {
+      $('#cv').html(data);
+    });
+  };
+  window.kb_source_2_profil = function() {
+    $.get('/pages/profil.html', function(data) {
+      $('#profil').html(data);
+    });
+  };
+  window.kb_source_2_leistungen = function() {
+    $.get('/pages/leistungen.html', function(data) {
+      $('#leistungen').html(data);
+    });
+  };
+  window.kb_source_2_portfolio = function() {
+    $.get('/pages/projekte.html', function(data) {
+      $('#projekte').html(data);
+    });
+  };
+  window.kb_source_2_bauenimbestand = function() {
+    $.get('/pages/bauenimbestand.html', function(data) {
+      $('#bauenimbestand').html(data);
+    });
+  };
+  window.kb_source_2_design = function() {
+    $.get('/pages/design.html', function(data) {
+      $('#design').html(data);
+    });
+  };
 });
 
 // dark_mode 
@@ -71,20 +84,3 @@ window.onload = function() {
     };
   }
 };
-
-// Lightbox Hider on any click outside
-$(document).ready(function() {
-  $('#fade').on('click', function(event) {
-    $(".white_content, #fade").hide();
-  });	
-});
-
-// Radio Button Closer: Accordion-Handling für Öffnen/Schließen
-$(document).ready(function(){
-  $("input:radio:checked").data("chk",true);
-  $("input:radio").click(function(){
-    $("input[name='"+$(this).attr("name")+"']:radio").not(this).removeData("chk");
-    $(this).data("chk",!$(this).data("chk"));
-    $(this).prop("checked",$(this).data("chk"));
-  });
-});
