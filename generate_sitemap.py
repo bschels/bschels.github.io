6c4b5edc620d55903a53fd2d56392a7177c8b755
Benni <benni.schels@gmail.com>
2026-01-02 23:47:08 +0100

    Neuer Artikel: Neubau – Planung, Ablauf & Besonderheiten

--- generate_sitemap.py
+++ generate_sitemap.py
@@ -28,6 +28,7 @@ PRIORITIES = {
     "/artikel/fachplaner.html": 0.8,
     "/artikel/bauweisen-neubau.html": 0.8,
     "/artikel/holzbau.html": 0.8,
+    "/artikel/neubau.html": 0.8,
     "/pages/impressum.html": 0.3,
     "/pages/datenschutz.html": 0.3,
 }
